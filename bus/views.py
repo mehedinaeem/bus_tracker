1cbe8ea3f4123c7421e2cab066aa33c51adf58e6
mehedinaeem <mehedinaeem00@gmail.com>
2025-01-11 13:30:27 +0600

    Added normal login/signup setup

--- bus/views.py
+++ bus/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
 
-# Create your views here.
+@login_required
+def home(request):
+    return render(request, 'home.html')
+
+def authView(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST or None)
+        if form.is_valid():
+            user = form.save()
+            # Automatically log the user in after signup
+            login(request, user)
+            return redirect('home')  # Redirect to home after successful signup
+    else:
+        form = UserCreationForm()
+    return render(request, 'bus/registration/signup.html', {"form": form})
--- bus_tracker/settings.py
+++ bus_tracker/settings.py
@@ -125,3 +125,9 @@ STATICFILES_DIRS=[
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = 'home'  # After successful login, redirect to home
+# LOGOUT_REDIRECT_URL = 'login'  # After logout, redirect to login page
+LOGOUT_REDIRECT_URL = '/accounts/login/'  # Adjust as needed
+
